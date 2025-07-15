e5a4391f29da99c749dfc5f39a0dc936c4fd0a1b
brainless <bimaarianto19@gmail.com>
2025-07-15 15:27:21 +0700

    Add AITools component to Home page for showcasing AI tools and features

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
+import AITools from "../components/AITools";
 
 const Home = () => {
   return (
     <>
       <Navbar />
       <Hero />
+      <AITools />
     </>
   );
 };
