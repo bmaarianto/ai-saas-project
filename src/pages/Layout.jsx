d6e5e4cae2545f5958f506fa4a97d69abba35ef2
brainless <bimaarianto19@gmail.com>
2025-07-16 20:27:40 +0700

    Add Sidebar component for navigation and user profile management

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,9 +1,46 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import { assets } from "../assets/assets";
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
+import Sidebar from "../components/Sidebar";
+import { SignIn, useUser } from "@clerk/clerk-react";
 
 const Layout = () => {
-  return (
-    <div>
-      <Outlet />
+  const navigate = useNavigate();
+  const [sidebar, setSidebar] = useState(false);
+  const { user } = useUser();
+
+  return user ? (
+    <div className="flex flex-col items-start justify-start h-screen">
+      <nav className="w-full px-8 min-h-14 flex items-center justify-between border-b border-gray-200">
+        <img
+          src={assets.logo}
+          alt="logo"
+          onClick={() => navigate("/")}
+          className="cursor-pointer w-32 sm:sm-44"
+        />
+        {sidebar ? (
+          <X
+            className="w-6 h-6 text-gray-600 sm:hidden"
+            onClick={() => setSidebar(false)}
+          />
+        ) : (
+          <Menu
+            className="w-6 h-6 text-gray-600 sm:hidden"
+            onClick={() => setSidebar(true)}
+          />
+        )}
+      </nav>
+      <div className="flex-1 w-full flex h-[calc(100vh-64px)]">
+        <Sidebar sidebar={sidebar} setSidebar={setSidebar} />
+        <div className="flex-1 bg-[#f4f7fb]">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  ) : (
+    <div className="flex items-center justify-center h-screen">
+      <SignIn />
     </div>
   );
 };
